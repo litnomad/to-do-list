c451ea4034e93e75bec8a70ad73f4a2b40dff17a
litnomad <128262375+litnomad@users.noreply.github.com>
2026-01-03 18:05:35 -0500

    add addEventListener to button for pop up display of form. create and append project DOM elements to page after user hits submit. Created project will display a button to add new tasks which will store it in an array.

--- src/script.js
+++ src/script.js
@@ -1,16 +1,123 @@
 import "./style.css";
 
-const addButton = document.querySelector('.open-button');
+const main = document.querySelector('#main');
+const sidebar = document.querySelector('#sidebar');
+const addProject = document.querySelector('.open-button');
 const closeButton = document.querySelector('.btnCancel');
+const submitButton = document.querySelector('button[type="submit"]');
 
 const displayForm = () => document.querySelector(".form-popup").style.display = "block";
 const hideForm = () => document.querySelector(".form-popup").style.display = "none";
 
-addButton.addEventListener('click', (event) => {
+addProject.addEventListener('click', (event) => {
     displayForm();
-
-    // Prevent default action of the button
     event.preventDefault();
 })
 
 closeButton.addEventListener('click', (event) => hideForm())
+
+const createProject = () => {
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const input = document.querySelector('input[id=title]').value;
+    const button = document.createElement('button');
+
+    div.setAttribute('class', 'card');
+    button.setAttribute('class', 'toDoButton');
+
+    h3.innerText = input;
+    button.innerText = 'add to do list';
+
+    main.appendChild(div);
+    div.appendChild(h3);
+    div.appendChild(button);
+
+    button.addEventListener('click', (event) => {
+        toDoForm();
+        event.preventDefault();
+    });
+
+}
+
+submitButton.addEventListener('click', (event) => {
+    createProject();
+    hideForm();
+    event.preventDefault();
+})
+
+const list = [];
+
+class createTask {
+    constructor(title, description, deadline, priority) {
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+    }
+}
+
+const toDoForm = () => {
+    const div = document.createElement('div');
+    const form = document.createElement('form');
+    const title = document.createElement('label');
+    const description = document.createElement('label');
+    const deadline = document.createElement('label');
+    const priority = document.createElement('label');
+    const inputTitle = document.createElement('input');
+    const inputDescription = document.createElement('textarea');
+    const inputDeadline = document.createElement('input');
+    const inputPriority = document.createElement('select');
+    const highPriority = document.createElement('option');
+    const medPriority = document.createElement('option');
+    const lowPriority = document.createElement('option');
+    const submit = document.createElement('button');
+
+    div.setAttribute('class', 'toDoContainer');
+    form.setAttribute('class', 'toDoForm');
+
+    inputDeadline.setAttribute('type', 'date');
+
+    title.setAttribute('for', 'title');
+    description.setAttribute('for', 'description');
+    deadline.setAttribute('for', 'deadline');
+    priority.setAttribute('for', 'priority');
+
+    title.innerText = 'title:';
+    description.innerText = 'description:';
+    deadline.innerText = 'deadline:';
+    priority.innerText = 'priority:'
+    highPriority.innerText = 'high';
+    medPriority.innerText = 'medium';
+    lowPriority.innerText = 'low';
+    submit.innerText = 'submit';
+
+    inputTitle.setAttribute('id', 'title');
+    inputDescription.setAttribute('id', 'description');
+    inputDeadline.setAttribute('id', 'deadline');
+    inputPriority.setAttribute('id', 'priority');
+    highPriority.setAttribute('value', 'high');
+    medPriority.setAttribute('value', 'medium');
+    lowPriority.setAttribute('value', 'low');
+
+    main.appendChild(form);
+    form.appendChild(title);
+    form.appendChild(inputTitle);
+    form.appendChild(description);
+    form.appendChild(inputDescription);
+    form.appendChild(deadline);
+    form.appendChild(inputDeadline);
+    form.appendChild(priority);
+    form.appendChild(inputPriority);
+    inputPriority.appendChild(highPriority);
+    inputPriority.appendChild(medPriority);
+    inputPriority.appendChild(lowPriority);
+    form.appendChild(submit);
+
+    submit.addEventListener('click', (event) => {
+        let task = new createTask(inputTitle.value, inputDescription.value, inputDeadline.value, inputPriority.value);
+        list.push(task);
+        console.log(ToDoList);
+        event.preventDefault()
+    });
+
+}
