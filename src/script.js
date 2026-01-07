81f603a4a9b269241235d616006a5360b3caff7c
litnomad <128262375+litnomad@users.noreply.github.com>
2026-01-07 10:37:32 -0500

    revise code so that project div elements have an id and add it to tasks associated with the project. new tasks are now attached to their projects. the displayList() function selects for the project id and appends the tasks. loadStorage() function is added.

--- src/script.js
+++ src/script.js
@@ -18,33 +18,7 @@ addProject.addEventListener('click', (event) => {
 
 closeButton.addEventListener('click', (event) => hideForm())
 
-// how project will be displayed on page
-
-const createProject = () => {
-    const div = document.createElement('div');
-    const h3 = document.createElement('h3');
-    const title = document.querySelector('input[id=title]').value;
-    const button = document.createElement('button');
-    const id = window.crypto.randomUUID();
-
-    div.id = id;
-    div.classList.add(id); // how do I create a unique class for each project?
-
-    h3.innerText = title;
-    button.innerText = 'add to do list';
-
-    main.appendChild(div);
-    div.appendChild(h3);
-    div.appendChild(button);
-
-    button.addEventListener('click', (event) => {
-        toDoForm();
-        event.preventDefault();
-    });
-
-}
-
-// submit button creates new project
+// pop up submit button creates new project
 
 submitButton.addEventListener('click', (event) => {
     createProject();
@@ -52,53 +26,164 @@ submitButton.addEventListener('click', (event) => {
     event.preventDefault();
 })
 
+// how project will be displayed on page
+
+const createProject = () => {
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const projectTitle = document.querySelector('input[id=title]').value;
+    const button = document.createElement('button');
+    const id = window.crypto.randomUUID();
+
+    div.setAttribute('id', id);
+
+    console.log(div.getAttribute('id'));
+
+    h3.innerText = projectTitle;
+    button.innerText = 'add to do list';
+    button.setAttribute('type', 'submit');
+
+    main.appendChild(div);
+    div.appendChild(h3);
+    div.appendChild(button);
+
+    button.addEventListener('click', (event) => {
+        toDoForm(id, projectTitle);
+        event.preventDefault();
+    });
+}
+
 // creates new to do list items 
 
 let list = [];
 
 class createTask {
-    constructor(title, description, deadline, priority) {
+    constructor(title, description, deadline, priority, projectTitle, id) {
         this.title = title;
         this.description = description;
         this.deadline = deadline;
         this.priority = priority;
+        this.project = projectTitle;
+        this.id = id;
     }
 }
 
-// To Do List display
+// Display list array 
 
-function displayList() {
+function displayList(task) {
 
-    for (const item of list) {
+    // attach tasks to their project cards
 
-        const project = document.querySelector(`.${item.title}`); // Mixing up task with project title //
+    if (document.getElementById(task.id)) {
+
+        const project = document.getElementById(`${task.id}`);
         const div = document.createElement('div');
         const title = document.createElement('p');
         const description = document.createElement('p');
-        const deadline = document.createElement('p');
+        const inputDeadline = document.createElement('input');
+        const deleteButton = document.createElement('button');
+
+        // create dropdown selection for priority
         const inputPriority = document.createElement('select');
-        const highPriority = document.createElement('option');
-        const mediumPriority = document.createElement('option');
-        const lowPriority = document.createElement('option');
+        const high = document.createElement('option');
+        const medium = document.createElement('option');
+        const low = document.createElement('option');
 
-        inputPriority.setAttribute('class', '.inputPriority');
-        /* document.querySelector('.inputPriority').value = 'list.inputPriority'; */
+        high.setAttribute('value', 'high');
+        medium.setAttribute('value', 'medium');
+        low.setAttribute('value', 'low');
 
-        title.innerText = item.title;
-        description.innerText = item.description;
-        deadline.innerText = item.deadline;
+        high.textContent = 'high';
+        medium.textContent = 'medium';
+        low.textContent = 'low';
+
+        inputPriority.appendChild(high);
+        inputPriority.appendChild(medium);
+        inputPriority.appendChild(low);
+
+        inputPriority.setAttribute('name', 'priority');
+        inputPriority.value = task.priority;
+
+        inputDeadline.setAttribute('type', 'date');
+        inputDeadline.setAttribute('value', task.deadline);
+
+        title.textContent = task.title;
+        description.textContent = task.description;
+        deleteButton.textContent = 'delete';
 
         project.appendChild(div);
         div.appendChild(title);
         div.appendChild(description);
+        div.appendChild(inputDeadline);
+        div.appendChild(inputPriority);
+        div.appendChild(deleteButton);
 
+        deleteButton.addEventListener('click', (event) => {
+            project.removeChild(div);
+
+        });
     }
 
 }
 
+
+/* To Do List display
+
+function displayList(task, id) {
+
+    const project = document.getElementById(`${id}`);
+    const div = document.createElement('div');
+    const title = document.createElement('p');
+    const description = document.createElement('p');
+    const inputDeadline = document.createElement('input');
+    const deleteButton = document.createElement('button');
+
+    // create dropdown selection for priority
+    const inputPriority = document.createElement('select');
+    const high = document.createElement('option');
+    const medium = document.createElement('option');
+    const low = document.createElement('option');
+
+    high.setAttribute('value', 'high');
+    medium.setAttribute('value', 'medium');
+    low.setAttribute('value', 'low');
+
+    high.textContent = 'high';
+    medium.textContent = 'medium';
+    low.textContent = 'low';
+
+    inputPriority.appendChild(high);
+    inputPriority.appendChild(medium);
+    inputPriority.appendChild(low);
+
+    inputPriority.setAttribute('name', 'priority');
+    inputPriority.value = task.priority;
+
+    inputDeadline.setAttribute('type', 'date');
+    inputDeadline.setAttribute('value', task.deadline);
+
+    title.textContent = task.title;
+    description.textContent = task.description;
+    deleteButton.textContent = 'delete';
+
+    project.appendChild(div);
+    div.appendChild(title);
+    div.appendChild(description);
+    div.appendChild(inputDeadline);
+    div.appendChild(inputPriority);
+    div.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', (event) => {
+        project.removeChild(div);
+
+    });
+
+}
+*/
+
 // To Do List form  
 
-const toDoForm = () => {
+const toDoForm = (id, projectTitle) => {
     const div = document.createElement('div');
     const form = document.createElement('form');
     const title = document.createElement('label');
@@ -157,17 +242,22 @@ const toDoForm = () => {
 
     // submit button adds new items to To Do List
     submit.addEventListener('click', (event) => {
-        let task = new createTask(inputTitle.value, inputDescription.value, inputDeadline.value, inputPriority.value);
+        let task = new createTask(inputTitle.value, inputDescription.value, inputDeadline.value, inputPriority.value, projectTitle, id);
         list.push(task);
         console.log(list);
 
         storeList();
-        displayList();
+        displayList(task, id);
 
-        event.preventDefault()
+        event.preventDefault();
     });
 
 }
+
+// hide to do form after submission
+
+
+// local storage and retrieval of lists 
 
 // stores list as JSON string in local storage
 function storeList() {
@@ -181,8 +271,154 @@ function retrieveList() {
     list = JSON.parse(string);
 }
 
-/* retrieveList(); 
-console.log(list)
-console.log(list[1].title);
-*/
+retrieveList();
+console.log(list);
+
+
+function loadStorage() {
+
+    if (!list) {
+        return false;
+    }
+
+    for (const item of list) {
+        if (!document.getElementById(item.id)) {
+
+            // create projects
+            const div = document.createElement('div');
+            const h3 = document.createElement('h3');
+            const projectTitle = item.project;
+            const button = document.createElement('button');
+            const id = item.id;
+
+            console.log(id);
+            div.setAttribute('id', id);
+            console.log(div.getAttribute('id'))
+
+            h3.innerText = projectTitle;
+            button.innerText = 'add to do list';
+            button.setAttribute('type', 'submit');
+
+            // create to do list
+            const div2 = document.createElement('div');
+            const title = document.createElement('p');
+            const description = document.createElement('p');
+            const inputDeadline = document.createElement('input');
+            const deleteButton = document.createElement('button');
+
+            // create dropdown selection for priority
+            const inputPriority = document.createElement('select');
+            const high = document.createElement('option');
+            const medium = document.createElement('option');
+            const low = document.createElement('option');
+
+            high.setAttribute('value', 'high');
+            medium.setAttribute('value', 'medium');
+            low.setAttribute('value', 'low');
+
+            high.textContent = 'high';
+            medium.textContent = 'medium';
+            low.textContent = 'low';
+
+            inputPriority.appendChild(high);
+            inputPriority.appendChild(medium);
+            inputPriority.appendChild(low);
+
+            inputPriority.setAttribute('name', 'priority');
+            inputPriority.value = item.priority;
+
+            inputDeadline.setAttribute('type', 'date');
+            inputDeadline.setAttribute('name', 'deadline');
+            inputDeadline.setAttribute('value', item.deadline);
+
+            title.textContent = item.title;
+            description.textContent = item.description;
+            deleteButton.textContent = 'delete'
+
+            main.appendChild(div);
+            div.appendChild(h3);
+            div.appendChild(button);
+            div.appendChild(div2);
+            div2.appendChild(title);
+            div2.appendChild(description);
+            div2.appendChild(inputDeadline);
+            div2.appendChild(inputPriority);
+            div2.appendChild(deleteButton);
+
+            button.addEventListener('click', (event) => {
+                toDoForm(id, projectTitle);
+                event.preventDefault();
+            });
+
+            deleteButton.addEventListener('click', () => {
+                div.removeChild(div2);
+                list = list.filter((obj) => obj != item);
+                console.log(list);
+                storeList();
+            });
+        }
+        else {
+            if (document.getElementById(item.id)) {
+
+                const project = document.getElementById(`${item.id}`);
+                const div = document.createElement('div');
+                const title = document.createElement('p');
+                const description = document.createElement('p');
+                const inputDeadline = document.createElement('input');
+                const deleteButton = document.createElement('button');
+
+                // create dropdown selection for priority
+                const inputPriority = document.createElement('select');
+                const high = document.createElement('option');
+                const medium = document.createElement('option');
+                const low = document.createElement('option');
+
+                high.setAttribute('value', 'high');
+                medium.setAttribute('value', 'medium');
+                low.setAttribute('value', 'low');
+
+                high.textContent = 'high';
+                medium.textContent = 'medium';
+                low.textContent = 'low';
+
+                inputPriority.appendChild(high);
+                inputPriority.appendChild(medium);
+                inputPriority.appendChild(low);
+
+                inputPriority.setAttribute('name', 'priority');
+                inputPriority.value = item.priority;
+
+                inputDeadline.setAttribute('type', 'date');
+                inputDeadline.setAttribute('value', item.deadline);
+
+                // set the task div items to equal item.id or random number?
+                // delete button 
+                // set onclick attribute for delete button to call deleteTask()
+                // deleteTask()
+                // if div.id == this.id (button) then getElementbyID(this.id).remove()
+
+                title.textContent = item.title;
+                description.textContent = item.description;
+                deleteButton.textContent = 'delete';
+
+                project.appendChild(div);
+                div.appendChild(title);
+                div.appendChild(description);
+                div.appendChild(inputDeadline);
+                div.appendChild(inputPriority);
+                div.appendChild(deleteButton);
+
+                deleteButton.addEventListener('click', () => {
+                    project.removeChild(div);
+                    list = list.filter((obj) => obj != item);
+                    console.log(list);
+                    storeList();
+                });
+            }
+        }
+    }
+}
+
+loadStorage();
+
 
